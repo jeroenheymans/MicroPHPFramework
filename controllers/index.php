65f880732f4e9020c87b1b2dae9487c23b2319e9
Jeroen Heymans <jeroen.heymans@live.be>
2011-07-29 22:21:40 +0200

    Extending the second hello world test with view

--- controllers/index.php
+++ controllers/index.php
@@ -1,6 +1,11 @@
 <?php
+require_once 'views/index.php';
 
 class IndexController extends Controller {
+
+	public function __construct() {
+		$this->view = new IndexView();
+	}
 
 	public function index() {
 		echo 'Second Hello World!';
