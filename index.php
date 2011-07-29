36ba9528a8a572fcfe7448dca6b338887566fb9a
Jeroen Heymans <jeroen.heymans@live.be>
2011-07-29 20:17:01 +0200

    Added doc

--- index.php
+++ index.php
@@ -11,5 +11,6 @@ require_once 'application/model.php';
 require_once 'application/view.php';
 
 // determine what needs to be loaded and start appropriate controller and action (for the moment just the basic one)
+// TODO: require_once the correct controller and call the correct action
 $c = new Controller();
 $c->index();
